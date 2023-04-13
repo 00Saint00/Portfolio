2f3da6682405ece84ea658b5ac6590c23cf9cf78
00Saint00 <paulotomewo3@gmail.com>
2023-04-13 13:17:56 +0100

    changed title

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -12,14 +12,14 @@ const Banner = ({ handleOpen }) => {
       id="home"
     >
       <div className="container mx-auto">
-        <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center">
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center">
           <div className="flex-1 text-center font-secondary lg:text-left">
             <motion.h1
               variants={fadeIn("up", 0.4)}
               initial="hidden"
               whileInView={"show"}
               viewport={{ once: false, amount: 0.7 }}
-              className="text-[33px] font-bold leading-[0.8] lg:text-[65px]"
+              className="text-[33px] font-bold leading-[0.8] lg:text-[65px] mb-6"
             >
               Hi,
             </motion.h1>
@@ -28,7 +28,7 @@ const Banner = ({ handleOpen }) => {
               initial="hidden"
               whileInView={"show"}
               viewport={{ once: false, amount: 0.7 }}
-              className="mb-3 text-[35px] ig:text-[50px] font-secondary font-semibold uppercase loading-[1]"
+              className="mb-5 text-[35px] ig:text-[50px] font-secondary font-semibold uppercase loading-[1]"
             >
               <TypeAnimation
                 speed={30}
